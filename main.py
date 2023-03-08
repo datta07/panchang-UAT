8bd5c8ef68d9932b9546a80c3ffabfb7d6101838
datta07 <akula.gurudatta@gmail.com>
2023-03-09 00:03:29 +0530

    changed lat and lng

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ app=FastAPI()
 #http://127.0.0.1:8000/panchang?date=28-07-2000&alt=0&lng=16.015066&lat=80.825353
 #uvicorn main:app --host 0.0.0.0 --port 10000
 @app.get("/panchang")
-def panchang(date=time.strftime("%d-%m-%Y"),lat="75.7764300",lng="23.1823900",alt="0.494"):
+def panchang(date=time.strftime("%d-%m-%Y"),lat="23.1823900",lng="75.7764300",alt="0.494"):
 	date=date.split("-")
-	return Panchang(int(date[0]),int(date[1]),int(date[2]),lng,lat,alt).data
+	return Panchang(int(date[0]),int(date[1]),int(date[2]),lat,lng,alt).data
